24dcf6e7fe11ca64349fac0266f5c24cdef3957e
n8761 <n8761@student.jamk.fi>
2024-06-29 11:07:38 +0300

    done up to Exercise_12.12

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,30 +1,48 @@
-const express = require('express');
+const express = require('express')
 const { Todo } = require('../mongo')
-const router = express.Router();
+const router = express.Router()
+const { getAsync, setAsync } = require('../redis')
+const { get } = require('mongoose')
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
   const todos = await Todo.find({})
-  console.log('häh')
-  res.send(todos);
-});
+  res.send(todos)
+})
 
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
+    done: false,
   })
-  res.send(todo);
-});
+  res.send(todo)
 
-const singleRouter = express.Router();
+  // Retrieve the current count or initialize it to 0 if not present
+  let addedTodos = await getAsync('added_todos')
+
+  addedTodos = Number(addedTodos)
+  if (isNaN(addedTodos)) {
+    console.error(
+      'Expected a numeric value for added_todos, but got:',
+      addedTodos
+    )
+    addedTodos = 0
+  }
+
+  // Increment and update the added_todos in Redis
+  addedTodos += 1
+  await setAsync('added_todos', addedTodos)
+  console.log('Added todos:', addedTodos)
+})
+
+const singleRouter = express.Router()
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
   console.log(id)
   req.todo = await Todo.findById(id)
-  console.log('----------',req.todo)
+  console.log('----------', req.todo)
   if (!req.todo) return res.sendStatus(404)
 
   next()
@@ -32,29 +50,38 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
-});
+  await req.todo.delete()
+  res.sendStatus(200)
+})
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
   console.log(req)
-  res.send(req.todo);
-  
+  res.send(req.todo)
+
   // res.sendStatus(405); // Implement this
-});
+})
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
   req.todo.text = req.body.text
   req.todo.done = req.body.done
   await req.todo.save()
-  res.send(req.todo);
+  res.send(req.todo)
 
   // res.sendStatus(405); // Implement this
-});
+})
+
+// GET statistics at /statistics, which returns the count of added todos
+router.get('/statistics', async (req, res) => {
+  let added_todos = await getAsync('added_todos')
+  added_todos = parseInt(added_todos, 10)
+  if (isNaN(added_todos)) {
+    added_todos = 0
+  }
+  res.send({ added_todos })
+})
 
 router.use('/:id', findByIdMiddleware, singleRouter)
 
-
-module.exports = router;
+module.exports = router
